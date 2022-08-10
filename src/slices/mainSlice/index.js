02c8d6f1c285bbe390086cb0f11ba684fea911fe
SridharDnesh <sridharan7614@gmail.com>
2022-08-10 22:51:54 +0530

    Add: Action for timesheet app

--- src/slices/mainSlice/index.js
+++ src/slices/mainSlice/index.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import {
   defaultReject,
   defaultState,
+  __deleteUrl__,
   __readDocumentUrl__,
   __uspsertUrl__,
 } from "../../settings";
@@ -32,14 +33,170 @@ const GET_USER_BY_USERNAME_PASSWORD = createAsyncThunk(
   }
 );
 
+// TASK - Employee level
+const UPSERT_TASK = createAsyncThunk(
+  `mainSlice/upsertTask`,
+  async (payload = {}, { rejectWithValue }) => {
+    try {
+      const Body = mainSliceQuery.upsertTask(payload);
+      const data = await fetchData(
+        { body: JSON.stringify(Body) },
+        __uspsertUrl__
+      );
+      return {
+        ...defaultState.Info,
+        data,
+      };
+    } catch (error) {
+      return rejectWithValue({
+        ...defaultReject,
+        message: error.message,
+      });
+    }
+  }
+);
+
+const DELETE_TASK = createAsyncThunk(
+  `mainSlice/deleteTask`,
+  async (payload = {}, { rejectWithValue }) => {
+    try {
+      const Body = mainSliceQuery.deleteTask(payload);
+      const data = await fetchData(
+        { body: JSON.stringify(Body) },
+        __deleteUrl__
+      );
+      return {
+        ...defaultState.Info,
+        data,
+      };
+    } catch (error) {
+      return rejectWithValue({
+        ...defaultReject,
+        message: error.message,
+      });
+    }
+  }
+);
+
+const GET_TASK_BY_ID = createAsyncThunk(
+  `mainSlice/getTaskById`,
+  async (payload = {}, { rejectWithValue }) => {
+    try {
+      const Body = mainSliceQuery.getTaskByTaskId(payload);
+      const data = await fetchData(
+        { body: JSON.stringify(Body) },
+        __readDocumentUrl__
+      );
+      return {
+        ...defaultState.List,
+        data,
+      };
+    } catch (error) {
+      return rejectWithValue({
+        ...defaultReject,
+        message: error.message,
+      });
+    }
+  }
+);
+
+const GET_TASKS_BY_EMPLOYEE_ID = createAsyncThunk(
+  `mainSlice/getTasksByEmployeeId`,
+  async (payload = {}, { rejectWithValue }) => {
+    try {
+      const data = await fetchData(
+        {
+          body: JSON.stringify(mainSliceQuery.getTaskByEmployeeId(payload)),
+        },
+        __readDocumentUrl__
+      );
+
+      return {
+        ...defaultState.List,
+        data: data,
+      };
+    } catch (error) {
+      return rejectWithValue({
+        ...defaultReject,
+        message: error.message,
+      });
+    }
+  }
+);
+
+const GET_ALL_TASKS = createAsyncThunk(
+  `mainSlice/getAllTasks`,
+  async (payload = {}, { rejectWithValue }) => {
+    try {
+      const data = await fetchData(
+        {
+          body: JSON.stringify(mainSliceQuery.getAllTasks()),
+        },
+        __readDocumentUrl__
+      );
+
+      return {
+        ...defaultState.List,
+        data: data,
+      };
+    } catch (error) {
+      return rejectWithValue({
+        ...defaultReject,
+        message: error.message,
+      });
+    }
+  }
+);
+
+const UPDATE_TASK_STATUS = createAsyncThunk(
+  `mainSlice/updateTaskStatus`,
+  async (payload = {}, { rejectWithValue }) => {
+    try {
+      const Body = mainSliceQuery.updateTaskStatus(payload);
+      const data = await fetchData(
+        { body: JSON.stringify(Body) },
+        __uspsertUrl__
+      );
+      return {
+        ...defaultState.Info,
+        data,
+      };
+    } catch (error) {
+      return rejectWithValue({
+        ...defaultReject,
+        message: error.message,
+      });
+    }
+  }
+);
+
 const mainSlice = createSlice({
   name: "mainSlice",
   initialState: {
     getUserByUsernamePassword: {
       ...defaultState.List,
     },
+    upsertTask: {
+      ...defaultState.Info,
+    },
+    deleteTask: {
+      ...defaultState.Info,
+    },
+    getTaskById: {
+      ...defaultState.Info,
+    },
+    getTasksByEmployeeId: {
+      ...defaultState.List,
+    },
+    getAllTasks: {
+      ...defaultState.List,
+    },
+    updateTaskStatus: {
+      ...defaultState.Info,
+    },
   },
   extraReducers: {
+    // GET_USER_BY_USERNAME_PASSWORD
     [GET_USER_BY_USERNAME_PASSWORD.fulfilled]: (state, action) => {
       state.getUserByUsernamePassword = action?.payload ?? {};
     },
@@ -49,12 +206,84 @@ const mainSlice = createSlice({
     [GET_USER_BY_USERNAME_PASSWORD.rejected]: (state, action) => {
       state.getUserByUsernamePassword = action?.payload ?? {};
     },
+
+    // UPSERT_TASK
+    [UPSERT_TASK.fulfilled]: (state, action) => {
+      state.upsertTask = action?.payload ?? {};
+    },
+    [UPSERT_TASK.pending]: (state, action) => {
+      state.upsertTask.loading = true;
+    },
+    [UPSERT_TASK.rejected]: (state, action) => {
+      state.upsertTask = action?.payload ?? {};
+    },
+
+    // DELETE_TASK
+    [DELETE_TASK.fulfilled]: (state, action) => {
+      state.deleteTask = action?.payload ?? {};
+    },
+    [DELETE_TASK.pending]: (state, action) => {
+      state.deleteTask.loading = true;
+    },
+    [DELETE_TASK.rejected]: (state, action) => {
+      state.deleteTask = action?.payload ?? {};
+    },
+
+    // GET_TASK_BY_ID
+    [GET_TASK_BY_ID.fulfilled]: (state, action) => {
+      state.getTaskById = action?.payload ?? {};
+    },
+    [GET_TASK_BY_ID.pending]: (state, action) => {
+      state.getTaskById.loading = true;
+    },
+    [GET_TASK_BY_ID.rejected]: (state, action) => {
+      state.getTaskById = action?.payload ?? {};
+    },
+
+    // GET_TASKS_BY_EMPLOYEE_ID
+    [GET_TASKS_BY_EMPLOYEE_ID.fulfilled]: (state, action) => {
+      state.getTasksByEmployeeId = action?.payload ?? {};
+    },
+    [GET_TASKS_BY_EMPLOYEE_ID.pending]: (state, action) => {
+      state.getTasksByEmployeeId.loading = true;
+    },
+    [GET_TASKS_BY_EMPLOYEE_ID.rejected]: (state, action) => {
+      state.getTasksByEmployeeId = action?.payload ?? {};
+    },
+
+    // GET_ALL_TASKS
+    [GET_ALL_TASKS.fulfilled]: (state, action) => {
+      state.getAllTasks = action?.payload ?? {};
+    },
+    [GET_ALL_TASKS.pending]: (state, action) => {
+      state.getAllTasks.loading = true;
+    },
+    [GET_ALL_TASKS.rejected]: (state, action) => {
+      state.getAllTasks = action?.payload ?? {};
+    },
+
+    // UPDATE_TASK_STATUS
+    [UPDATE_TASK_STATUS.fulfilled]: (state, action) => {
+      state.updateTaskStatus = action?.payload ?? {};
+    },
+    [UPDATE_TASK_STATUS.pending]: (state, action) => {
+      state.updateTaskStatus.loading = true;
+    },
+    [UPDATE_TASK_STATUS.rejected]: (state, action) => {
+      state.updateTaskStatus = action?.payload ?? {};
+    },
   },
 });
 
 // ORDER_SET ACTION LISTS
 const mainSliceActions = {
   GET_USER_BY_USERNAME_PASSWORD,
+  UPSERT_TASK,
+  DELETE_TASK,
+  GET_TASK_BY_ID,
+  GET_TASKS_BY_EMPLOYEE_ID,
+  GET_ALL_TASKS,
+  UPDATE_TASK_STATUS,
 };
 
 export { mainSliceActions };
--- src/slices/mainSlice/query.js
+++ src/slices/mainSlice/query.js
@@ -10,4 +10,86 @@ export const mainSliceQuery = {
       return_fields: "user",
     };
   },
+
+  upsertTask: (data) => {
+    let filter = {};
+    if (data?._key) {
+      filter = { filter: { _key: data?._key } };
+    }
+    return [
+      {
+        db_name: dbName,
+        entity: "TaskDetails",
+        metadata_dbname: "ATP_Platform_DEV",
+        is_metadata: true,
+        metadataId: "6aa05483-3a2f-4c1c-af4c-7abfc925a5dd",
+        ...filter,
+        doc: {
+          title: data?.title,
+          empID: data?.empID,
+          duration: Number(data?.duration),
+        },
+      },
+    ];
+  },
+
+  deleteTask: ({ _key }) => {
+    return {
+      db_name: dbName,
+      entity: "TaskDetails",
+      metadata_dbname: "ATP_Platform_DEV",
+      filter: `TaskDetails._key=='${_key}'`,
+    };
+  },
+
+  getTaskByTaskId: ({ taskKey }) => {
+    return {
+      db_name: dbName,
+      metadata_dbname: "ATP_Platform_DEV",
+      filter: `TaskDetails._key=='${taskKey}'`,
+      entity: "TaskDetails",
+      return_fields: "TaskDetails",
+    };
+  },
+
+  getTaskByEmployeeId: ({ empID }) => {
+    return {
+      db_name: dbName,
+      metadata_dbname: "ATP_Platform_DEV",
+      entity: "TaskDetails",
+      filter: `TaskDetails.empID=='${empID}'`,
+      return_fields: "TaskDetails",
+    };
+  },
+
+  getAllTasks: () => {
+    return {
+      db_name: dbName,
+      metadata_dbname: "ATP_Platform_DEV",
+      entity: "TaskDetails",
+      return_fields: "merge(TaskDetails, {empID: document(TaskDetails.empID)})",
+      sort: "TaskDetails.updatedate DESC",
+    };
+  },
+
+  updateTaskStatus: ({ _key, status, verifiedby, reason = "" }) => {
+    debugger;
+    return [
+      {
+        db_name: dbName,
+        entity: "TaskDetails",
+        metadata_dbname: "ATP_Platform_DEV",
+        is_metadata: true,
+        metadataId: "6aa05483-3a2f-4c1c-af4c-7abfc925a5dd",
+        filter: {
+          _key: _key,
+        },
+        doc: {
+          status: status,
+          verifiedby: verifiedby,
+          reason: reason,
+        },
+      },
+    ];
+  },
 };
